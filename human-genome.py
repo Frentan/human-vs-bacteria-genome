b300890d5cff7168178d0260ac099d74ef86b1e6
Tulio Geniole <62348349+Frentan@users.noreply.github.com>
2020-04-28 21:43:21 -0300

    Version 1.0

---
+++ human-genome.py
@@ -0,0 +1,24 @@
+input = open("human.fasta").read()
+output = open("human.html","w")
+
+count = {}
+
+for i in ['A', 'T', 'C', 'G']:
+    for j in ['A', 'T', 'C', 'G']:
+        count[i+j] = 0
+
+input = input.replace("\n","")
+
+for k in range(len(input)-1):
+    count[input[k]+input[k+1]] += 1
+
+i = 1
+for k in count:
+	transparency = count[k]/max(count.values())
+	output.write("<div style='width:100px; border:1px solid #111; color:#fff; height:100px; float:left; background-color:rgba(0, 75, 0, "+str(transparency)+"')>"+k+"</div>")
+
+	if i%4 == 0:
+		output.write("<div style='clear:both'></div>")
+	i+=1
+
+output.close()
